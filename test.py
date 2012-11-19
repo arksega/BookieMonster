41739f779196fca95bdff3ed17cdd1b8cd2a9627
Sergio Gutierrez Alvarez <ark.sega@gmail.com>
2012-11-19 16:34:42 -0600

    Creation and use of Sphere class
    The unidad variable changed to speed

--- test.py
+++ test.py
@@ -2,6 +2,38 @@ import pyglet
 from pyglet.gl import *
 from pyglet.window import key
 
+class Sphere():
+    def __init__(self, red = 0.0, green = 0.0, blue = 0.0, radius = 5.0, slices = 12, stacks = 12):
+        self.red    = red
+        self.green  = green
+        self.blue   = blue
+        self.radius = radius
+        self.slices = slices
+        self.stacks = stacks
+        self.q = gluNewQuadric()
+        self.posX = 0.0
+        self.posY = 0.0
+        self.deltaX = 0.0
+        self.deltaY = 0.0
+
+    def draw(self):
+        self.move()
+        glColor3f(self.red, self.green, self.blue)
+        #gluQuadricDrawStyle(q,GLU_LINE)
+        gluQuadricDrawStyle(self.q,GLU_FILL)
+        glPushMatrix()
+        glTranslatef(self.posX, self.posY, 0.0)
+        gluSphere(self.q, self.radius, self.slices, self.stacks)
+        glPopMatrix()
+
+    def move(self):
+        self.posX += self.deltaX
+        self.posY += self.deltaY
+
+    def stop(self):
+        self.deltaX = 0.0
+        self.deltaY = 0.0
+
 class Board(pyglet.window.Window):
 
     def __init__(self):
@@ -10,17 +42,13 @@ class Board(pyglet.window.Window):
         self.focus = [0.0, 0.0, 0.0]
         self.up    = [0.0, 1.0, 0]
         self.currentParameter = self.eye
-        self.unidad = 1.0
-        self.deltaX = 0.0
-        self.deltaY = 0.0
-        self.posX = 0.0
-        self.posY = 0.0
-        self.posZ = 0.0
+        self.speed = 1.0
         #One-time GL setup
         glClearColor(1, 1, 1, 1)
         glColor3f(1, 0, 0)
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_CULL_FACE)
+        self.monster = Sphere(0.0, 0.0, 0.1)
 
         # Uncomment this line for a wireframe view
         # glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
@@ -55,23 +83,7 @@ class Board(pyglet.window.Window):
         batch.draw()
 
         #batch = pyglet.graphics.Batch()
-        self.drawSphere()
-
-    def drawSphere(self):
-        self.moveSphere()
-        # Draw sphere
-        glColor3f(0.0, 0.0, 0.0)
-        q = gluNewQuadric()
-        #gluQuadricDrawStyle(q,GLU_LINE)
-        gluQuadricDrawStyle(q,GLU_FILL)
-        glPushMatrix()
-        glTranslatef(self.posX, self.posY, 0.0)
-        gluSphere(q,5.0,20,20)
-        glPopMatrix()
-
-    def moveSphere(self):
-        self.posX += self.deltaX
-        self.posY += self.deltaY
+        self.monster.draw()
     
     def on_resize(self, width, height):
         glViewport(0,0,self.width,self.height)
@@ -92,42 +104,44 @@ class Board(pyglet.window.Window):
         glLoadIdentity()
         if symbol == key.A:
             self.currentParameter = self.eye
-            self.unidad = 1.0
+            self.speed = 1.0
         elif symbol == key.B:
             self.currentParameter = self.focus
-            self.unidad = 1.0
+            self.speed = 1.0
         elif symbol == key.C:
-            self.unidad = 0.1
+            self.speed = 0.1
             self.currentParameter = self.up
         elif symbol == key.NUM_7:
-            self.currentParameter[0] = round(self.currentParameter[0] + self.unidad,1)
+            self.currentParameter[0] = round(self.currentParameter[0] + self.speed,1)
         elif symbol == key.NUM_8:
-            self.currentParameter[1] = round(self.currentParameter[1] + self.unidad,1)
+            self.currentParameter[1] = round(self.currentParameter[1] + self.speed,1)
         elif symbol == key.NUM_9:
-            self.currentParameter[2] = round(self.currentParameter[2] + self.unidad,1)
+            self.currentParameter[2] = round(self.currentParameter[2] + self.speed,1)
         elif symbol == key.NUM_1:
-            self.currentParameter[0] = round(self.currentParameter[0] - self.unidad,1)
+            self.currentParameter[0] = round(self.currentParameter[0] - self.speed,1)
         elif symbol == key.NUM_2:
-            self.currentParameter[1] = round(self.currentParameter[1] - self.unidad,1)
+            self.currentParameter[1] = round(self.currentParameter[1] - self.speed,1)
         elif symbol == key.NUM_3:
-            self.currentParameter[2] = round(self.currentParameter[2] - self.unidad,1)
+            self.currentParameter[2] = round(self.currentParameter[2] - self.speed,1)
         elif symbol == key.RIGHT:
-            self.deltaX = 1.0
-            self.deltaY = 0.0
+            self.monster.deltaX = 1.0
+            self.monster.deltaY = 0.0
         elif symbol == key.LEFT:
-            self.deltaX = -1.0
-            self.deltaY = 0.0
+            self.monster.deltaX = -1.0
+            self.monster.deltaY = 0.0
         elif symbol == key.UP:
-            self.deltaY = 1.0
-            self.deltaX = 0.0
+            self.monster.deltaY = 1.0
+            self.monster.deltaX = 0.0
         elif symbol == key.DOWN:
-            self.deltaY = -1.0
-            self.deltaX = 0.0
+            self.monster.deltaY = -1.0
+            self.monster.deltaX = 0.0
+        elif symbol == key.SPACE:
+            self.monster.stop()
 
         print(self.eye + self.focus + self.up)
         self.initView()
         return pyglet.event.EVENT_HANDLED
     
-win   = Board()
+win = Board()
 
 pyglet.app.run()
