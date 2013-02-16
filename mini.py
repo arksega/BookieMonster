f27559377f654a7b667de7ceeb2ccc93c20e566d
Sergio <ark.sega@gmail.com>
2013-02-16 12:23:35 -0600

    Canvas 3D

--- mini.py
+++ mini.py
@@ -24,6 +24,46 @@ class GLWidget(QGLWidget):
     def __init__(self, parent=None):
         QGLWidget.__init__(self, parent)
 
+    def initializeGL(self):
+        glClearColor(1, 1, 1, 1);
+        glColor3f(1, 0, 0)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_CULL_FACE)
+
+    def resizeGL(self, w, h):
+        glViewport(0, 0, w, h)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(60., w / float(h), .1, 1000.)
+        gluLookAt(-100, 100, 100, 0, 0, 0, 0, 0, 1)
+        glMatrixMode(GL_MODELVIEW)
+        #glLoadIdentity()
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.draw_grid()
+
+    def draw_grid(self):
+        self.alfa = 10
+        self.beta = 2
+        self.gridSize = 10
+        self.unit = self.beta + self.alfa
+        glBegin(GL_LINES)
+        glColor3f(0.0, 0.0, 0.0)
+        end = self.unit * self.gridSize + self.alfa / 2
+        step = self.alfa + self.beta
+        for i in range(0, self.gridSize * 2 + 2):
+            glVertex3i(end - i * step, -end - self.beta, 0)
+            glVertex3i(end - i * step,  end + self.beta, 0)
+            glVertex3i(end - i * step + self.beta, -end - self.beta, 0)
+            glVertex3i(end - i * step + self.beta,  end + self.beta, 0)
+
+            glVertex3i(-end - self.beta, end - i * step, 0)
+            glVertex3i(end + self.beta, end - i * step, 0)
+            glVertex3i(-end - self.beta, end - i * step + self.beta, 0)
+            glVertex3i(end + self.beta, end - i * step + self.beta, 0)
+        glEnd()
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
