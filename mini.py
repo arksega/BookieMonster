05114dab7e9f014e1778c7b7ac6cbb7be448af77
celakanto <celakanto@linux-9cm5.site>
2013-02-17 00:46:15 -0600

    2D over 3D, static

--- mini.py
+++ mini.py
@@ -25,15 +25,21 @@ class GLWidget(QGLWidget):
         QGLWidget.__init__(self, parent)
 
     def initializeGL(self):
+        glDisable(GL_TEXTURE_2D);
+        glDisable(GL_DEPTH_TEST);
+        glDisable(GL_COLOR_MATERIAL);
+        glEnable(GL_BLEND);
+        glEnable(GL_POLYGON_SMOOTH);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
         glClearColor(1, 1, 1, 1);
-        glColor3f(1, 0, 0)
-        glEnable(GL_DEPTH_TEST)
-        glEnable(GL_CULL_FACE)
 
     def resizeGL(self, w, h):
         glViewport(0, 0, w, h)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         self.setup3D(w, h)
         self.draw_grid()
+        self.setup2D(w, h)
+        self.draw_triangle()
 
     def setup3D(self, w, h):
         glMatrixMode(GL_PROJECTION)
@@ -42,10 +48,25 @@ class GLWidget(QGLWidget):
         gluLookAt(-100, 100, 100, 0, 0, 0, 0, 0, 1)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    def setup2D(self, w, h):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluOrtho2D(0, w, 0, h)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
 
     def paintGL(self):
+        print('draw')
         pass
+
+    def draw_triangle(self):
+        glColor3f(1,0,0)
+        glBegin(GL_POLYGON)
+        glVertex2f(0,0)
+        glVertex2f(100,500)
+        glVertex2f(500,100)
+        glEnd()
 
     def draw_grid(self):
         self.alfa = 10
